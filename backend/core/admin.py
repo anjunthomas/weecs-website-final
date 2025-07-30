40a1f7b240d28e6ea6ac75bd24c27bb81c0a7c88
Anju Thomas <anjunoblethomas@gmail.com>
2025-07-30 01:39:27 -0400

    styled and animated svgs for homepage

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,4 @@
-from django.contrib import admin
-
-# Register your models here.
+#admin.py
 from django.contrib import admin
 from .models import Event
 
--- frontend/weecs-react-calendar/tailwind.config.js
+++ frontend/weecs-react-calendar/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,css}" 
+  ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
+  extend: {
+    textShadow: {
+      glow: '0 0 2px #c084fc, 0 0 4px #c084fc, 0 0 7px #9333ea',
+    }
+  }
+},
+  plugins: [
+    require('tailwindcss-textshadow')
+  ],
 }
