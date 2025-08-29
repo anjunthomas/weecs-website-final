fa8afd1c2669f99e14722e811186ec3ab0302a4f
anjunthomas <anjunoblethomas@gmail.com>
2025-08-29 01:32:44 -0400

    added chameleon to about section, animated with hover and blinking

--- frontend/weecs-react-calendar/src/About.jsx
+++ frontend/weecs-react-calendar/src/About.jsx
@@ -1,7 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './styles/About.css';
+import './styles/LightningBolt.css';
+
+
 
 function About(){
+    const [isBlinking, setIsBlinking] = useState(false);
+    useEffect(() => {
+        const blinkInterval = setInterval(() => {
+            setIsBlinking(true);
+            setTimeout(() => setIsBlinking(false), 200); // Blink for 200ms
+        }, 4000); // Blink every 4 seconds
+
+        return () => clearInterval(blinkInterval);
+    }, []);
+
     return(
         <section id="about-route" className="about-card"> {/* id is necessary to make the page scroll to "about" from the navbar*/}
             <div className="about-section">
@@ -13,9 +26,14 @@ function About(){
                     If any of these interests you, WEECS is the place for you! 
                     <br></br>Check out upcoming events in the calendar above and join us for our next meeting. We can't wait to see you there!
                 </p>
-            </div>  
-            
+                <img 
+                    src={isBlinking ? "/static/react/assets/ChameleonBlink.png" : "/static/react/assets/Chameleon.png"} 
+                    alt="Weecs Chameleon" 
+                    className="chameleon" 
+                /> 
+            </div>     
         </section>
+        
        
     );
 }
