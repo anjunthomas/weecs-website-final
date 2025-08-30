4b756a68f9df15a5bde0b286c522e0c636dc4e90
anjunthomas <anjunoblethomas@gmail.com>
2025-08-30 17:55:23 -0400

    updating chameleon, homepage, and footer styles for mobile responsiveness

--- backend/core/static/scripts/typewriter.js
+++ backend/core/static/scripts/typewriter.js
@@ -1,12 +1,18 @@
 
-  const text = `Uplifting women in EECS with hands on software and hardware projects, workshops, and mentorship!`;
+  const text = `Uplifting women in EECS with hands on software and hardware projects, \nworkshops, and mentorship!`;
 
   const target = document.getElementById('typewriter');
   let index = 0;
 
   function typeChar() {
     if (index < text.length) {
-      target.innerHTML += text.charAt(index);
+      const char = text.charAt(index);
+      if (char === '\n') {
+        target.innerHTML += '<br>';
+      } else {
+        
+        target.innerHTML += char;
+      }
       index++;
       setTimeout(typeChar, 30); // Typing speed in ms
     }
